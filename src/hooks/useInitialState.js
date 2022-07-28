91338699ae65461497245912afbb687ff41e5133
Bassignani Martin <bassignanimartin@icloud.com>
2022-07-28 09:24:33 -0300

    Primer commit

---
+++ src/hooks/useInitialState.js
@@ -0,0 +1,32 @@
+import { useState } from "react";
+
+const initialState = {
+    cart: [], 
+}
+
+const useInitialSatate = () => {
+    
+    const [state, setState] = useState(initialState);
+
+    const addToCart = (payload) => {
+        setState({
+            ...state,
+            cart: [...state.cart, payload]
+        });
+    };
+
+	const removeFromCart = (payload) => {
+		setState({
+			...state,
+			cart: state.cart.filter(items => items.id !== payload.id),
+		});
+	}
+
+	return {
+		state,
+		addToCart,
+		removeFromCart
+	}
+}
+
+export default useInitialSatate;
